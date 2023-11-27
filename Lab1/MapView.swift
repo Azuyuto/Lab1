3a4a91317d4cf72edddf43d60fcbfe06e50f9eb8
Dawid Mazur <dawid.m@webchefs.pl>
2023-11-27 13:02:15 +0100

    Lab 4 i 5

---
+++ Lab1/MapView.swift
@@ -0,0 +1,8 @@
+//
+//  MapView.swift
+//  Lab1
+//
+//  Created by Dawid on 27/11/2023.
+//
+
+import Foundation
---
+++ Lab1/Store/BuildingStore.swift
@@ -0,0 +1,8 @@
+//
+//  BuildingStore.swift
+//  Lab1
+//
+//  Created by Dawid on 27/11/2023.
+//
+
+import Foundation
